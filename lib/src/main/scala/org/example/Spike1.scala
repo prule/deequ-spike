705f6f73eb18800b7af5b0551e98f3cefd75b70c
paulrule <paulrule1@gmail.com>
2024-07-16 18:28:57 +1000

    version where verification passes

--- lib/src/main/scala/org/example/Spike1.scala
+++ lib/src/main/scala/org/example/Spike1.scala
@@ -11,55 +11,51 @@ class Spike1 {
 
   // function to print hello
   def go(): Unit = {
-    println("Hello there!")
-  }
 
-  // taken from https://github.com/awslabs/deequ/blob/master/src/main/scala/com/amazon/deequ/examples/KLLCheckExample.scala
-  ExampleUtils.withSpark { session =>
-    println(s"Spark version: ${session.version}")
+    // taken from https://github.com/awslabs/deequ/blob/master/src/main/scala/com/amazon/deequ/examples/KLLCheckExample.scala
+    ExampleUtils.withSpark { session =>
+      println(s"Spark version: ${session.version}")
 
-    val data = itemsAsDataframe(session,
-      Item(1, "Thingy A", "awesome thing.", "high", 0),
-      Item(2, "Thingy B", "available at http://thingb.com", null, 0),
-      Item(3, null, null, "low", 5),
-      Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
-      Item(5, "Thingy E", null, "high", 9))
+      val data = itemsAsDataframe(session,
+        Item(1, "Thingy A", "awesome thing.", "high", 0),
+        Item(2, "Thingy B", "available at http://thingb.com", null, 0),
+        Item(3, null, null, "low", 5),
+        Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
+        Item(5, "Thingy E", null, "high", 10))
 
-    data.show()
+      data.show()
 
-    val newData = data.select(data("numViews").cast(DoubleType).as("numViews"))
+      val newData = data.select(data("numViews").cast(DoubleType).as("numViews"))
 
-    val verificationResult = VerificationSuite()
-      .onData(newData)
-      .addCheck(
-        Check(CheckLevel.Error, "integrity checks")
-          // we expect 5 records
-          .hasSize(_ == 6)
-          // we expect the maximum of tips to be not more than 10
-          .hasMax("numViews", _ <= 8)
-          // we expect the sketch size to be at least 16
-          .kllSketchSatisfies("numViews", _.parameters(1) >= 16,
-            kllParameters = Option(KLLParameters(2, 0.64, 2)))
-      )
-      .run()
+      val verificationResult = VerificationSuite()
+        .onData(newData)
+        .addCheck(
+          Check(CheckLevel.Error, "integrity checks")
+            // we expect 5 records
+            .hasSize(_ == 5)
+            // we expect the maximum of tips to be not more than 10
+            .hasMax("numViews", _ <= 10)
+        ).run()
 
-    if (verificationResult.status == CheckStatus.Success) {
-      println("The data passed the test, everything is fine!")
-    } else {
-      println("We found errors in the data, the following constraints were not satisfied:\n")
+//      The data passed the test, everything is fine!
 
-      val resultsForAllConstraints = verificationResult.checkResults
-        .flatMap { case (_, checkResult) => checkResult.constraintResults }
+      if (verificationResult.status == CheckStatus.Success) {
+        println("The data passed the test, everything is fine!")
+      } else {
+        println("We found errors in the data, the following constraints were not satisfied:\n")
 
-      resultsForAllConstraints
-        .filter {
-          _.status != ConstraintStatus.Success
-        }
-        .foreach { result =>
-          println(s"${result.constraint} failed: ${result.message.get}")
-        }
+        val resultsForAllConstraints = verificationResult.checkResults
+          .flatMap { case (_, checkResult) => checkResult.constraintResults }
+
+        resultsForAllConstraints
+          .filter {
+            _.status != ConstraintStatus.Success
+          }
+          .foreach { result =>
+            println(s"${result.constraint} failed: ${result.message.get}")
+          }
+      }
+
     }
-
   }
-
 }
